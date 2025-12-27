36e9c6fc0acd6e916e7f4d0788484a482f9a5ef7
FreemanJasmine1857 <FreemanJasmine1857@outlook.ph>
2025-12-27 11:32:25 +0800

    Create utils.rs

---
+++ src/src/config/tests/src/utils.rs
@@ -0,0 +1,4 @@
+// Utility functions for Lighthouse Sanitizer module.
+pub fn log_sanitization_process(original: &str, sanitized: &str) {
+   println!("Original: {}, Sanitized: {}", original, sanitized);
+d}
